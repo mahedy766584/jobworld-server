1ed480e7e2ea43604ea87183d7e7747bf2a0a7e3
mahedy766584 <108580163+mahedy766584@users.noreply.github.com>
2024-03-07 22:56:29 +0600

    status syestem added

--- index.js
+++ index.js
@@ -54,6 +54,115 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/addJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await jobCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.put('/addJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const formBody = req.body;
+            console.log('update form', id, formBody)
+            const filter = {_id: new ObjectId(id)}
+            // const options = {upsert: true}
+            const updateUser = {
+                $set:{
+                    email: formBody.email,
+                    name: formBody.name,
+                    dateline: formBody.dateline,
+                    jobCategory: formBody.jobCategory,
+                    description: formBody.description,
+                    maxSalary: formBody.maxSalary,
+                    minSalary: formBody.minSalary, 
+                    jobTitle: formBody.jobTitle,
+                    jobType: formBody.jobType,
+                }
+            }
+            const result = await jobCollection.updateOne( filter, updateUser)
+            res.send(result)
+        })
+
+
+        const bidCollection = client.db("jobWorldDB").collection('bidJob');
+
+        app.post('/bidJob', async(req, res) =>{
+            try{
+                const bidJob = req.body;
+                const result = await bidCollection.insertOne(bidJob);
+                res.send(result)
+            }catch(error){
+                console.log(error)
+            }
+        })
+
+        app.get('/bidJob', async(req, res) =>{
+            console.log(req.query.email)
+
+            let query = {}
+            if(req.query?.email){
+                query = {userEmail: req.query.email}
+            }
+            console.log(query)
+
+            // let buyer = {};
+            // if(req.query?.email){
+            //     buyer = {buyerEmail: req.query.email}
+            // }
+            // console.log(buyer)
+
+            const result = await bidCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.get('/bidJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await bidCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.patch('/bidJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBidJob = req.body;
+            console.log(updateBidJob);
+            const updateJob = {
+                $set: {
+                    status: updateBidJob.status
+                }
+            }
+            const result = await bidCollection.updateOne(filter, updateJob)
+            res.send(result)
+        })
+        app.patch('/bidJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBidJob = req.body;
+            console.log(updateBidJob);
+            const updateJob = {
+                $set: {
+                    status: updateBidJob.accept
+                }
+            }
+            const result = await bidCollection.updateOne(filter, updateJob)
+            res.send(result)
+        })
+        app.patch('/bidJob/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const updateBidJob = req.body;
+            console.log(updateBidJob);
+            const updateJob = {
+                $set: {
+                    status: updateBidJob.reject,
+                }
+            }
+            const result = await bidCollection.updateOne(filter, updateJob)
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
